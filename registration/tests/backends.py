d3aeb2a5fb9c81c501225a4e1a78321f21ab6e2c
Oleg Dolya <oleg.dolya@gmail.com>
2010-11-30 18:58:32 +0200

    merge modified:   .gitignore modified:   INSTALL modified:   LICENSE modified:   registration/__init__.py modified:   registration/backends/default/urls.py new file:   registration/backends/simple/__init__.py new file:   registration/backends/simple/urls.py modified:   registration/forms.py modified:   registration/models.py modified:   registration/tests/__init__.py modified:   registration/tests/backends.py

--- registration/tests/backends.py
+++ registration/tests/backends.py
@@ -3,6 +3,7 @@ import datetime
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.sites.models import Site
 from django.core import mail
 from django.core.exceptions import ImproperlyConfigured
@@ -15,6 +16,7 @@ from registration import signals
 from registration.admin import RegistrationAdmin
 from registration.backends import get_backend
 from registration.backends.default import DefaultBackend
+from registration.backends.simple import SimpleBackend
 from registration.models import RegistrationProfile
 
 
@@ -22,13 +24,13 @@ class _MockRequestClient(Client):
     """
     A ``django.test.Client`` subclass which can return mock
     ``HttpRequest`` objects.
-    
+
     """
     def request(self, **request):
         """
         Rather than issuing a request and returning the response, this
         simply constructs an ``HttpRequest`` object and returns it.
-        
+
         """
         environ = {
             'HTTP_COOKIE':      self.cookies,
@@ -49,7 +51,13 @@ class _MockRequestClient(Client):
             }
         environ.update(self.defaults)
         environ.update(request)
-        return WSGIRequest(environ)
+        request = WSGIRequest(environ)
+
+        # We have to manually add a session since we'll be bypassing
+        # the middleware chain.
+        session_middleware = SessionMiddleware()
+        session_middleware.process_request(request)
+        return request
 
 
 def _mock_request():
@@ -57,7 +65,7 @@ def _mock_request():
     Construct and return a mock ``HttpRequest`` object; this is used
     in testing backend methods which expect an ``HttpRequest`` but
     which are not being called from views.
-    
+
     """
     return _MockRequestClient().request()
 
@@ -90,7 +98,7 @@ class BackendRetrievalTests(TestCase):
         """
         Test that a backend module which exists but does not have a
         class of the specified name raises the correct exception.
-        
+
         """
         self.assertRaises(ImproperlyConfigured, get_backend,
                           'registration.backends.default.NonexistentBackend')
@@ -106,14 +114,14 @@ class DefaultRegistrationBackendTests(TestCase):
     the default backend.
 
     """
+    backend = DefaultBackend()
+
     def setUp(self):
         """
         Create an instance of the default backend for use in testing,
         and set ``ACCOUNT_ACTIVATION_DAYS`` if it's not set already.
 
         """
-        from registration.backends.default import DefaultBackend
-        self.backend = DefaultBackend()
         self.old_activation = getattr(settings, 'ACCOUNT_ACTIVATION_DAYS', None)
         if self.old_activation is None:
             settings.ACCOUNT_ACTIVATION_DAYS = 7
@@ -158,7 +166,7 @@ class DefaultRegistrationBackendTests(TestCase):
         Test that registration still functions properly when
         ``django.contrib.sites`` is not installed; the fallback will
         be a ``RequestSite`` instance.
-        
+
         """
         Site._meta.installed = False
 
@@ -175,7 +183,7 @@ class DefaultRegistrationBackendTests(TestCase):
 
         self.assertEqual(RegistrationProfile.objects.count(), 1)
         self.assertEqual(len(mail.outbox), 1)
-        
+
         Site._meta.installed = True
 
     def test_valid_activation(self):
@@ -255,7 +263,7 @@ class DefaultRegistrationBackendTests(TestCase):
         """
         Test that registering a user sends the ``user_registered``
         signal.
-        
+
         """
         def receiver(sender, **kwargs):
             self.failUnless('user' in kwargs)
@@ -279,7 +287,7 @@ class DefaultRegistrationBackendTests(TestCase):
         """
         Test that successfully activating a user sends the
         ``user_activated`` signal.
-        
+
         """
         def receiver(sender, **kwargs):
             self.failUnless('user' in kwargs)
@@ -305,7 +313,7 @@ class DefaultRegistrationBackendTests(TestCase):
         """
         Test that an unsuccessful activation attempt does not send the
         ``user_activated`` signal.
-        
+
         """
         receiver = lambda sender, **kwargs: received_signals.append(kwargs.get('signal'))
 
@@ -326,19 +334,19 @@ class DefaultRegistrationBackendTests(TestCase):
     def test_email_send_action(self):
         """
         Test re-sending of activation emails via admin action.
-        
+
         """
         admin_class = RegistrationAdmin(RegistrationProfile, admin.site)
-        
+
         alice = self.backend.register(_mock_request(),
                                       username='alice',
                                       email='alice@example.com',
                                       password1='swordfish')
-        
+
         admin_class.resend_activation_email(_mock_request(),
                                             RegistrationProfile.objects.all())
         self.assertEqual(len(mail.outbox), 2) # One on registering, one more on the resend.
-        
+
         RegistrationProfile.objects.filter(user=alice).update(activation_key=RegistrationProfile.ACTIVATED)
         admin_class.resend_activation_email(_mock_request(),
                                             RegistrationProfile.objects.all())
@@ -347,7 +355,7 @@ class DefaultRegistrationBackendTests(TestCase):
     def test_activation_action(self):
         """
         Test manual activation of users view admin action.
-        
+
         """
         admin_class = RegistrationAdmin(RegistrationProfile, admin.site)
 
@@ -359,3 +367,110 @@ class DefaultRegistrationBackendTests(TestCase):
         admin_class.activate_users(_mock_request(),
                                    RegistrationProfile.objects.all())
         self.failUnless(User.objects.get(username='alice').is_active)
+
+
+class SimpleRegistrationBackendTests(TestCase):
+    """
+    Test the simple registration backend, which does signup and
+    immediate activation.
+
+    """
+    backend = SimpleBackend()
+
+    def test_registration(self):
+        """
+        Test the registration process: registration creates a new
+        inactive account and a new profile with activation key,
+        populates the correct account data and sends an activation
+        email.
+
+        """
+        new_user = self.backend.register(_mock_request(),
+                                         username='bob',
+                                         email='bob@example.com',
+                                         password1='secret')
+
+        # Details of the returned user must match what went in.
+        self.assertEqual(new_user.username, 'bob')
+        self.failUnless(new_user.check_password('secret'))
+        self.assertEqual(new_user.email, 'bob@example.com')
+
+        # New user must not be active.
+        self.failUnless(new_user.is_active)
+
+    def test_allow(self):
+        """
+        Test that the setting ``REGISTRATION_OPEN`` appropriately
+        controls whether registration is permitted.
+
+        """
+        old_allowed = getattr(settings, 'REGISTRATION_OPEN', True)
+        settings.REGISTRATION_OPEN = True
+        self.failUnless(self.backend.registration_allowed(_mock_request()))
+
+        settings.REGISTRATION_OPEN = False
+        self.failIf(self.backend.registration_allowed(_mock_request()))
+        settings.REGISTRATION_OPEN = old_allowed
+
+    def test_form_class(self):
+        """
+        Test that the default form class returned is
+        ``registration.forms.RegistrationForm``.
+
+        """
+        self.failUnless(self.backend.get_form_class(_mock_request()) is forms.RegistrationForm)
+
+    def test_post_registration_redirect(self):
+        """
+        Test that the default post-registration redirect is the public
+        URL of the new user account.
+
+        """
+        new_user = self.backend.register(_mock_request(),
+                                         username='bob',
+                                         email='bob@example.com',
+                                         password1='secret')
+
+        self.assertEqual(self.backend.post_registration_redirect(_mock_request(), new_user),
+                         (new_user.get_absolute_url(), (), {}))
+
+    def test_registration_signal(self):
+        """
+        Test that registering a user sends the ``user_registered``
+        signal.
+
+        """
+        def receiver(sender, **kwargs):
+            self.failUnless('user' in kwargs)
+            self.assertEqual(kwargs['user'].username, 'bob')
+            self.failUnless('request' in kwargs)
+            self.failUnless(isinstance(kwargs['request'], WSGIRequest))
+            received_signals.append(kwargs.get('signal'))
+
+        received_signals = []
+        signals.user_registered.connect(receiver, sender=self.backend.__class__)
+
+        self.backend.register(_mock_request(),
+                              username='bob',
+                              email='bob@example.com',
+                              password1='secret')
+
+        self.assertEqual(len(received_signals), 1)
+        self.assertEqual(received_signals, [signals.user_registered])
+
+    def test_activation(self):
+        """
+        Test that activating against this backend is an error.
+
+        """
+        self.assertRaises(NotImplementedError, self.backend.activate,
+                          request=_mock_request())
+
+    def test_post_activation_redirect(self):
+        """
+        Test that asking for a post-activation redirect from this
+        backend is an error.
+
+        """
+        self.assertRaises(NotImplementedError, self.backend.post_activation_redirect,
+                          request=_mock_request(), user=User())

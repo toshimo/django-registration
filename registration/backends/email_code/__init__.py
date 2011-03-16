1ab3bc3d2e5ad7bce59de3cd47ad67d76d9ad5ba
Chris Streeter <chris@chrisstreeter.com>
2011-03-16 15:23:07 -0700

    Whitespacing.

--- registration/backends/email_code/__init__.py
+++ registration/backends/email_code/__init__.py
@@ -13,36 +13,36 @@ from signup_codes.models import SignupCode
 class EmailCodeBackend(object):
     """
     A registration backend which follows a simple workflow:
-
+    
     1. User signs up (require signup code), inactive account is created.
-
+    
     2. Email is sent to user with activation link.
-
+    
     3. User clicks activation link, account is now active.
-
+    
     Using this backend requires that
-
+    
     * ``registration`` be listed in the ``INSTALLED_APPS`` setting
       (since this backend makes use of models defined in this
       application).
-
+    
     * The setting ``ACCOUNT_ACTIVATION_DAYS`` be supplied, specifying
       (as an integer) the number of days from registration during
       which a user may activate their account (after that period
       expires, activation will be disallowed).
-
+    
     * The creation of the templates
       ``registration/activation_email_subject.txt`` and
       ``registration/activation_email.txt``, which will be used for
       the activation email. See the notes for this backends
       ``register`` method for details regarding these templates.
-
+    
     Additionally, registration can be temporarily closed by adding the
     setting ``REGISTRATION_OPEN`` and setting it to
     ``False``. Omitting this setting, or setting it to ``True``, will
     be interpreted as meaning that registration is currently open and
     permitted.
-
+    
     Internally, this is accomplished via storing an activation key in
     an instance of ``registration.models.RegistrationProfile``. See
     that model and its custom manager for full documentation of its
@@ -53,27 +53,27 @@ class EmailCodeBackend(object):
         """
         Given a username, email address and password, register a new
         user account, which will initially be inactive.
-
+        
         Along with the new ``User`` object, a new
         ``registration.models.RegistrationProfile`` will be created,
         tied to that ``User``, containing the activation key which
         will be used for this account.
-
+        
         An email will be sent to the supplied email address; this
         email should contain an activation link. The email will be
         rendered using two templates. See the documentation for
         ``RegistrationProfile.send_activation_email()`` for
         information about these templates and the contexts provided to
         them.
-
+        
         After the ``User`` and ``RegistrationProfile`` are created and
         the activation email is sent, the signal
         ``registration.signals.user_registered`` will be sent, with
         the new ``User`` as the keyword argument ``user`` and the
         class of this backend as the sender.
-
+        
         """
-
+        
         username = generate_unique_username()
         email, password = kwargs['email'], kwargs['password1']
         if Site._meta.installed:
@@ -85,18 +85,18 @@ class EmailCodeBackend(object):
         signals.user_registered.send(sender=self.__class__,
                                      user=new_user,
                                      request=request)
-
+        
         signup_code = kwargs["signup_code"]
         if type(signup_code) == type(SignupCode()):
             signup_code.use(new_user)
-
+        
         return new_user
-
+    
     def activate(self, request, activation_key):
         """
         Given an an activation key, look up and activate the user
         account corresponding to that key (if possible).
-
+        
         After successful activation, the signal
         ``registration.signals.user_activated`` will be sent, with the
         newly activated ``User`` as the keyword argument ``user`` and
@@ -109,22 +109,22 @@ class EmailCodeBackend(object):
                                         user=activated,
                                         request=request)
         return activated
-
+    
     def registration_allowed(self, request):
         """
         Indicate whether account registration is currently permitted,
         based on the value of the setting ``REGISTRATION_OPEN``. This
         is determined as follows:
-
+        
         * If ``REGISTRATION_OPEN`` is not specified in settings, or is
           set to ``True``, registration is permitted.
-
+        
         * If ``REGISTRATION_OPEN`` is both specified and set to
           ``False``, registration is not permitted.
         
         """
         return getattr(settings, 'REGISTRATION_OPEN', True)
-
+    
     def get_form_class(self, request):
         """
         Return the default form class used for user registration.
@@ -132,7 +132,7 @@ class EmailCodeBackend(object):
         """
         # return RegistrationForm
         return EmailCodeRegistrationForm
-
+    
     def post_registration_redirect(self, request, user):
         """
         Return the name of the URL to redirect to after successful
@@ -140,7 +140,7 @@ class EmailCodeBackend(object):
         
         """
         return ('registration_complete', (), {})
-
+    
     def post_activation_redirect(self, request, user):
         """
         Return the name of the URL to redirect to after successful
@@ -148,3 +148,4 @@ class EmailCodeBackend(object):
         
         """
         return ('registration_activation_complete', (), {})
+    
